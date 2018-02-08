a08cf11aa04452a0403b25359e39f0945e240e20
Nicholas D'Amico <nickalan82@icloud.com>
2018-02-08 17:47:25 -0500

    added missing .mp3 extension to files, reformatted data to be more readable

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,26 +28,54 @@ wat_pah_nanachat = Monastery.create(name: 'Wat Nong Pah Pong', address: 'Ubon, T
 ############################
 # Abhayagiri Teachers
 ############################
-ajahn_karuṇadhammo = Teacher.create(name: 'Ajahn Karuṇadhammo', profile_img: 'http://www.abhayagiri.org/media/images/authors/Ajahn%20Karunadhammo-small.jpg', monastery: abhayagiri)
-ajahn_passano = Teacher.create(name: 'Ajahn Pasanno', profile_img: 'http://www.abhayagiri.org/media/images/authors/Ajahn%20Pasanno-small.JPG', monastery: abhayagiri)
-ajahn_naniko = Teacher.create(name: 'Ajahn Ñāniko', profile_img: 'http://www.abhayagiri.org/media/images/authors/Ajahn%20Nyaniko-small.JPG', monastery: abhayagiri)
+ajahn_karuṇadhammo = Teacher.create(
+  name: 'Ajahn Karuṇadhammo',
+  profile_img: 'http://www.abhayagiri.org/media/images/authors/Ajahn%20Karunadhammo-small.jpg',
+  monastery: abhayagiri
+)
+ajahn_passano = Teacher.create(
+  name: 'Ajahn Pasanno',
+  profile_img: 'http://www.abhayagiri.org/media/images/authors/Ajahn%20Pasanno-small.JPG',
+  monastery: abhayagiri
+)
+ajahn_naniko = Teacher.create(
+  name: 'Ajahn Ñāniko',
+  profile_img: 'http://www.abhayagiri.org/media/images/authors/Ajahn%20Nyaniko-small.JPG',
+  monastery: abhayagiri
+)
 
 ############################
 # Amaravati Teachers
 ############################
-ajahn_sumedho = Teacher.create(name: 'Ajahn Sumedho', profile_img: 'http://www.bia.or.th/en/images/photo/Ajahn-Sumedho.jpg', monastery: amaravati)
-ajahn_candasiri = Teacher.create(name: 'Ajahn Candasiri', profile_img: 'https://cdn.amaravati.org/wp-content/uploads/2014/06/candasiri_bio2.jpg', monastery: amaravati)
+ajahn_sumedho = Teacher.create(
+  name: 'Ajahn Sumedho',
+  profile_img: 'http://www.bia.or.th/en/images/photo/Ajahn-Sumedho.jpg',
+  monastery: amaravati
+)
+ajahn_candasiri = Teacher.create(
+  name: 'Ajahn Candasiri',
+  profile_img: 'https://cdn.amaravati.org/wp-content/uploads/2014/06/candasiri_bio2.jpg',
+  monastery: amaravati
+)
 
 ############################
 # Aruna Ratanagiri Teachers
 ############################
-ajahn_munindo = Teacher.create(name: 'Ajahn Munindo', profile_img: 'http://www.bia.or.th/en/images/photo/Ajahn-Sumedho.jpg', monastery: aruna)
+ajahn_munindo = Teacher.create(
+  name: 'Ajahn Munindo',
+  profile_img: 'http://www.bia.or.th/en/images/photo/Ajahn-Sumedho.jpg',
+  monastery: aruna
+)
 
 
 ############################
 # Forest Hermitage Teachers
 ############################
-luang_por_khemadhammo = Teacher.create(name: 'Luang Por Khemadhammo', profile_img: 'https://pbs.twimg.com/profile_images/1673555382/CIMG4997A.jpg', monastery: forest_hermitage)
+luang_por_khemadhammo = Teacher.create(
+  name: 'Luang Por Khemadhammo',
+  profile_img: 'https://pbs.twimg.com/profile_images/1673555382/CIMG4997A.jpg',
+  monastery: forest_hermitage
+)
 
 
 ####################################################
@@ -66,29 +94,56 @@ tradition = Tag.create(name: 'Tradition')
 understanding = Tag.create(name: 'Understanding')
 four_noble_truths = Tag.create(name: 'Four Noble Truths')
 autobiography = Tag.create(name: 'Autobiography')
+
+
+
 ####################################################
 # Talks
 ####################################################
-
 
 # Seed for only 4 - 6 Monasteries
 ############################
 # Abhayagiri Talks
 ############################
-going_beneath = Talk.create(title: 'Going Beneath the Thought', description: 'Ajahn Karunadhammo discusses instructions on how to remove unwholesome, distracting or proliferating thoughts. He focuses on the fourth method which retraces the thought back to its origin. He suggests moving to the affective experience that is feeding the thought or story, and then to see the bare energetic experience in the body.', language: 'English', file_url: 'http://www.abhayagiri.org/media/audio/youtube/P8l6jgpnxxY.mp3', teacher: ajahn_karuṇadhammo)
+
+going_beneath = Talk.create(
+  title: 'Going Beneath the Thought',
+  description: 'Ajahn Karunadhammo discusses instructions on how to remove unwholesome, distracting or proliferating thoughts. He focuses on the fourth method which retraces the thought back to its origin. He suggests moving to the affective experience that is feeding the thought or story, and then to see the bare energetic experience in the body.',
+  language: 'English',
+  file_url: 'http://www.abhayagiri.org/media/audio/youtube/P8l6jgpnxxY.mp3',
+  teacher: ajahn_karuṇadhammo
+)
 going_beneath.tags << suffering
 going_beneath.tags << right_effort
 going_beneath.save
 
-connect_lineage =  Talk.create(title: 'Connecting with the Lineage', description: 'Ajahn Pasanno describes the recent visit to Thailand by members of the community to attend the celebration of the one hundredth year of Ajahn Chah’s birth.', language: 'English', file_url: 'http://www.abhayagiri.org/media/audio/youtube/3oi13Xeqeq8', teacher: ajahn_passano)
+connect_lineage =  Talk.create(
+  title: 'Connecting with the Lineage',
+  description: 'Ajahn Pasanno describes the recent visit to Thailand by members of the community to attend the celebration of the one hundredth year of Ajahn Chah’s birth.',
+  language: 'English',
+  file_url: 'http://www.abhayagiri.org/media/audio/youtube/3oi13Xeqeq8.mp3',
+  teacher: ajahn_passano
+)
 connect_lineage.tags << forest_tradition
 connect_lineage.save
 
-going_against_stream = Talk.create(title: 'Are we going against the stream?', description: ' He discusses the five hindrances to meditation and their antidotes which are the four protective meditations: contemplation of the body, metta, death, and the Buddha.', language: 'English', file_url: 'http://www.abhayagiri.org/media/audio/youtube/7zXgHV9WOfM', teacher: ajahn_naniko)
+going_against_stream = Talk.create(
+  title: 'Are we going against the stream?',
+  description: ' He discusses the five hindrances to meditation and their antidotes which are the four protective meditations: contemplation of the body, metta, death, and the Buddha.',
+  language: 'English',
+  file_url: 'http://www.abhayagiri.org/media/audio/youtube/7zXgHV9WOfM.mp3',
+  teacher: ajahn_naniko
+)
 going_against_stream.tags << suffering
 going_against_stream.save
 
-returning_abhayagiri = Talk.create(title: 'Returning to Abhayagiri', description: 'Returning from his time in Thailand, Ajahn Naniko offers reflections on overcoming negativity, meditating in India, uncertainty, and spiritual friendship.', language: 'English', file_url: 'http://www.abhayagiri.org/media/audio/youtube/gDRjNwmhgDU', teacher: ajahn_naniko)
+returning_abhayagiri = Talk.create(
+  title: 'Returning to Abhayagiri',
+  description: 'Returning from his time in Thailand, Ajahn Naniko offers reflections on overcoming negativity, meditating in India, uncertainty, and spiritual friendship.',
+  language: 'English',
+  file_url: 'http://www.abhayagiri.org/media/audio/youtube/gDRjNwmhgDU.mp3',
+  teacher: ajahn_naniko
+)
 returning_abhayagiri.tags << patience
 returning_abhayagiri.tags << spiritual_friendship
 returning_abhayagiri.save
@@ -96,9 +151,22 @@ returning_abhayagiri.save
 ############################
 # Amaravati Talks
 ############################
-duty_of_living = Talk.create(title: 'The Duty of Living', description: 'This talk was given on 1 June 2011 at Amaravati Buddhist Monastery.', language: 'English', file_url: 'https://cdn.amaravati.org/wp-content/uploads/2014/10/11/Amaravati-2011-49-The-Duty-of-Living.mp3', teacher: ajahn_candasiri)
 
-practising_dhamma =  Talk.create(title: 'Listening and Practising the Dhamma', description: 'This talk was given by Luang Por Sumedho on 18 May 2014, on Vesakha Puja day, at Amaravati Buddhist Monastery, UK.', language: 'English', file_url: 'https://cdn.amaravati.org/wp-content/uploads/2014/10/11/Amaravati_2014_0518_Listening_and_Practising_the_Dhamma.mp3', teacher: ajahn_sumedho)
+duty_of_living = Talk.create(
+  title: 'The Duty of Living',
+  description: 'This talk was given on 1 June 2011 at Amaravati Buddhist Monastery.',
+  language: 'English',
+  file_url: 'https://cdn.amaravati.org/wp-content/uploads/2014/10/11/Amaravati-2011-49-The-Duty-of-Living.mp3',
+  teacher: ajahn_candasiri
+)
+
+practising_dhamma =  Talk.create(
+  title: 'Listening and Practising the Dhamma',
+  description: 'This talk was given by Luang Por Sumedho on 18 May 2014, on Vesakha Puja day, at Amaravati Buddhist Monastery, UK.',
+  language: 'English',
+  file_url: 'https://cdn.amaravati.org/wp-content/uploads/2014/10/11/Amaravati_2014_0518_Listening_and_Practising_the_Dhamma.mp3',
+  teacher: ajahn_sumedho
+)
 practising_dhamma.tags << practicing_dhamma
 practising_dhamma.save
 
@@ -107,12 +175,23 @@ practising_dhamma.save
 # Aruna Talks
 ############################
 
-trust_distrust = Talk.create(title: 'Trust and Distrust', description: 'Talk on dealing with trust and distrust in our faith, by Luang Por Munindo on February 04, 2018', language: 'English', file_url: 'https://ratanagiri.org.uk/system/refinery/resources/2018/02/05/2tw1bkw1eu_Ajahn_Munindo_Trust_and_Distrust.mp3', teacher: ajahn_munindo)
+trust_distrust = Talk.create(
+  title: 'Trust and Distrust',
+  description: 'Talk on dealing with trust and distrust in our faith, by Luang Por Munindo on February 04, 2018',
+  language: 'English', file_url: 'https://ratanagiri.org.uk/system/refinery/resources/2018/02/05/2tw1bkw1eu_Ajahn_Munindo_Trust_and_Distrust.mp3',
+  teacher: ajahn_munindo
+)
 trust_distrust.tags << faith
 trust_distrust.tags << trust
 trust_distrust.save
 
-let_this_be_enough = Talk.create(title: 'Let this be enough', description: 'Talk on contentment, by Luang Por Munindo on December 31, 2017', language: 'English', file_url: 'https://ratanagiri.org.uk/system/refinery/resources/2018/01/01/9cli1yl9mc_Ajahn_Munindo_Let_THIS_Be_Enough.mp3', teacher: ajahn_munindo)
+let_this_be_enough = Talk.create(
+  title: 'Let this be enough',
+  description: 'Talk on contentment, by Luang Por Munindo on December 31, 2017',
+  language: 'English',
+  file_url: 'https://ratanagiri.org.uk/system/refinery/resources/2018/01/01/9cli1yl9mc_Ajahn_Munindo_Let_THIS_Be_Enough.mp3',
+  teacher: ajahn_munindo
+)
 let_this_be_enough.tags << understanding
 let_this_be_enough.tags << tradition
 let_this_be_enough.save
@@ -122,12 +201,24 @@ let_this_be_enough.save
 # Forest Hermitage Talks
 ############################
 
-fh_four_noble_truths = Talk.create(title: 'The Four Noble Truths', description: 'Talk on the Four Noble Truths, by Luang Por Khemadhammo on Januaray 04, 2010', language: 'English', file_url: 'https://foresthermitage.org.uk/wp-content/uploads/2009/12/LP-Interview-Part-1', teacher: luang_por_khemadhammo)
+fh_four_noble_truths = Talk.create(
+  title: 'The Four Noble Truths',
+  description: 'Talk on the Four Noble Truths, by Luang Por Khemadhammo on Januaray 04, 2010',
+  language: 'English',
+  file_url: 'https://foresthermitage.org.uk/wp-content/uploads/2009/12/LP-Interview-Part-1.mp3',
+  teacher: luang_por_khemadhammo
+)
 fh_four_noble_truths.tags << four_noble_truths
 fh_four_noble_truths.tags << trust
 fh_four_noble_truths.save
 
-life_story = Talk.create(title: 'Luang Por Khemadhammo\'s life story part: 1', description: 'The life of Luang Por Khemadhammo part: 1, by Luang Por Khemadhammo on December 01, 2009', language: 'English', file_url: 'https://foresthermitage.org.uk/wp-content/uploads/2009/12/LP-Interview-Part-1', teacher: luang_por_khemadhammo)
+life_story = Talk.create(
+  title: 'Luang Por Khemadhammo\'s life story part: 1',
+  description: 'The life of Luang Por Khemadhammo part: 1, by Luang Por Khemadhammo on December 01, 2009',
+  language: 'English',
+  file_url: 'https://foresthermitage.org.uk/wp-content/uploads/2009/12/LP-Interview-Part-1.mp3',
+  teacher: luang_por_khemadhammo
+)
 life_story.tags << autobiography
 life_story.tags << faith
 life_story.save
