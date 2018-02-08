665e4070ddeac44a4247810aff3a6e85172cf3f5
Nicholas D'Amico <nickalan82@icloud.com>
2018-02-08 12:55:34 -0500

    add data for models

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,5 +20,50 @@ santacittarama = Monastery.create(name: 'Santacittarama', address: 'Italy', site
 wat_nong_pah_pong = Monastery.create(name: 'Wat Nong Pah Pong', address: 'Thailand', site_url: 'https://forestsangha.org/community/monasteries/wat-nong-pah-pong', img_url: 'https://media-cdn.tripadvisor.com/media/photo-s/01/e7/d8/60/main-pagodas-at-wat-nong.jpg')
 wat_pah_nanachat = Monastery.create(name: 'Wat Nong Pah Pong', address: 'Ubon, Thailand', site_url: 'http://www.watpahnanachat.org/', img_url: 'http://sanitykeeper.com/wp-content/uploads/2017/04/Wat-Pah-Nanachat-aerial-view.jpg')
 
+####################################################
 # Teachers
+####################################################
+
+############################
+# Abhayagiri Teachers
+############################
 ajahn_karuṇadhammo = Teacher.create(name: 'Ajahn Karuṇadhammo', profile_img: 'http://www.abhayagiri.org/media/images/authors/Ajahn%20Karunadhammo-small.jpg', monastery: abhayagiri)
+ajahn_passano = Teacher.create(name: 'Ajahn Pasanno', profile_img: 'http://www.abhayagiri.org/media/images/authors/Ajahn%20Pasanno-small.JPG', monastery: abhayagiri)
+ajahn_naniko = Teacher.create(name: 'Ajahn Ñāniko', profile_img: 'http://www.abhayagiri.org/media/images/authors/Ajahn%20Nyaniko-small.JPG', monastery: abhayagiri)
+
+####################################################
+# Tags
+####################################################
+suffering = Tag.create(name: 'Suffering')
+right_effort = Tag.create(name: 'Right Effort')
+forest_tradition = Tag.create(name: 'Thai Forest Tradition')
+patience = Tag.create(name: 'Patience')
+determination = Tag.create(name: 'Determination')
+spiritual_friendship = Tag.create(name: 'Spiritual Friendship')
+
+####################################################
+# Talks
+####################################################
+
+
+# Seed for only 4 - 6 Monasteries 
+############################
+# Abhayagiri Talks
+############################
+going_beneath = Talk.create(title: 'Going Beneath the Thought', description: 'Ajahn Karunadhammo discusses instructions on how to remove unwholesome, distracting or proliferating thoughts. He focuses on the fourth method which retraces the thought back to its origin. He suggests moving to the affective experience that is feeding the thought or story, and then to see the bare energetic experience in the body.', language: 'English', file_url: 'http://www.abhayagiri.org/media/audio/youtube/P8l6jgpnxxY.mp3', teacher: ajahn_karuṇadhammo)
+going_beneath.tags << suffering
+going_beneath.tags << right_effort
+going_beneath.save
+
+connect_lineage =  Talk.create(title: 'Connecting with the Lineage', description: 'Ajahn Pasanno describes the recent visit to Thailand by members of the community to attend the celebration of the one hundredth year of Ajahn Chah’s birth.', language: 'English', file_url: 'http://www.abhayagiri.org/media/audio/youtube/3oi13Xeqeq8', teacher: ajahn_passano)
+connect_lineage.tags << forest_tradition
+connect_lineage.save
+
+going_against_stream = Talk.create(title: 'Are we going against the stream?', description: ' He discusses the five hindrances to meditation and their antidotes which are the four protective meditations: contemplation of the body, metta, death, and the Buddha.', language: 'English', file_url: 'http://www.abhayagiri.org/media/audio/youtube/7zXgHV9WOfM', teacher: ajahn_naniko)
+going_against_stream.tags << suffering
+going_against_stream.save
+
+returning_abhayagiri = Talk.create(title: 'Returning to Abhayagiri', description: 'Returning from his time in Thailand, Ajahn Naniko offers reflections on overcoming negativity, meditating in India, uncertainty, and spiritual friendship.', language: 'English', file_url: 'http://www.abhayagiri.org/media/audio/youtube/gDRjNwmhgDU', teacher: ajahn_naniko)
+returning_abhayagiri.tags << patience
+returning_abhayagiri.tags << spiritual_friendship
+returning_abhayagiri.save
