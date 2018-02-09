6631b280df4e5d0522f84efb7032961ea42273da
Nicholas D'Amico <nickalan82@icloud.com>
2018-02-09 11:32:56 -0500

    add :title attribute for serialized data return on Talk Model

--- app/serializers/talk_serializer.rb
+++ app/serializers/talk_serializer.rb
@@ -1,4 +1,4 @@
 class TalkSerializer < ActiveModel::Serializer
-  attributes :id, :description, :duration, :language, :file_url
+  attributes :id, :title, :description, :duration, :language, :file_url
   has_one :teacher, serializer: TalkTeacherSerializer
 end
