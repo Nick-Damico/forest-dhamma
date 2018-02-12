71b87fb3ec2f564fb03f82cf744700a52f1ba301
Nicholas D'Amico <nickalan82@icloud.com>
2018-02-12 11:19:32 -0500

    add container component MonasteriesList

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import Home from './components/home';
+import MonasteryList from './containers/monasteries-list';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Home />
+        <MonasteryList />
       </div>
     );
   }
