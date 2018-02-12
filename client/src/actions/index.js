9e3d554bd7c05072a7fdc643e3cd6e97058be55c
Nicholas D'Amico <nickalan82@icloud.com>
2018-02-12 18:09:42 -0500

    fixed fetch to use thunk format

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,18 +4,20 @@ require('es6-promise').polyfill();
 const ROOT_URL = 'http://localhost:3001/api/v1';
 export const FETCH_MONASTERIES = 'FETCH_MONASTERIES';
 export const FETCH_MONASTERY = 'FETCH_MONASTERY';
+export const RECEIVED_MONASTERIES = 'RECEIVED_MONASTERIES';
 
 // 'ROOT_URL/monasteries'
-
 export function fetchMonasteries() {
-  const url = `${ROOT_URL}/monasteries`;
-  const request = fetch(url)
-                    .then(response => response.json())
-                    .then(json => json.monasteries)
-  return {
-    type: FETCH_MONASTERIES,
-    payload: request
-  };
+  return (dispatch) => {
+    dispatch({type: FETCH_MONASTERIES})
+    fetch(`${ROOT_URL}/monasteries`)
+      .then(response => {
+        return response.json()
+      })
+      .then(json => {
+        dispatch({type: RECEIVED_MONASTERIES, payload: json.monasteries})
+      })
+  }
 }
 
 // 'ROOT_URL/monasteries/:id'
