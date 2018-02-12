5e9c1d2cd44a7c4870be7bb84fd5bb8a7316e448
Nicholas D'Amico <nickalan82@icloud.com>
2018-02-12 11:57:11 -0500

    rendering monastery model :name, :talks length from provided props of parent container MonasteryList

--- client/src/components/monastery-item.js
+++ client/src/components/monastery-item.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const MonasteryItem = (props) => {
+const MonasteryItem = ({ monastery}) => {
+  const { name, talks } = monastery;
   return (
     <div>
-      <h3>Title</h3>
-      <p>15 Talks</p>
+      <h3>{name}</h3>
+      <p>{talks.length} Talks</p>
       <button>Click</button>
     </div>
   )
