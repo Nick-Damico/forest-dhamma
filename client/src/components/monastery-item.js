ea6fecef82a36992d066a4748b3802b5b73fcbbe
Nicholas D'Amico <nickalan82@icloud.com>
2018-02-12 11:18:59 -0500

    add component MonasteryItem handle presentational data from MonasteriesList parent container

---
+++ client/src/components/monastery-item.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MonasteryItem = (props) => {
+  return (
+    <div>
+      <h3>Title</h3>
+      <p>15 Talks</p>
+      <button>Click</button>
+    </div>
+  )
+}
+
+export default MonasteryItem;
