50813eb393f5ec1ac9bfb524b9ee4a9a9b6eb06c
Nicholas D'Amico <nickalan82@icloud.com>
2018-02-13 00:57:11 -0500

    fetch from API with 'redux-thunk'

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,13 +3,14 @@ require('es6-promise').polyfill();
 
 const ROOT_URL = 'http://localhost:3001/api/v1';
 export const FETCH_MONASTERIES = 'FETCH_MONASTERIES';
-export const FETCH_MONASTERY = 'FETCH_MONASTERY';
 export const RECEIVED_MONASTERIES = 'RECEIVED_MONASTERIES';
+export const FETCH_MONASTERY = 'FETCH_MONASTERY';
+export const RECEIVED_MONASTERY = 'RECEIVED_MONASTERY';
 
 // 'ROOT_URL/monasteries'
 export function fetchMonasteries() {
   return (dispatch) => {
-    dispatch({type: FETCH_MONASTERIES})
+    dispatch({type: FETCH_MONASTERIES});
     fetch(`${ROOT_URL}/monasteries`)
       .then(response => {
         return response.json()
@@ -17,13 +18,21 @@ export function fetchMonasteries() {
       .then(json => {
         dispatch({type: RECEIVED_MONASTERIES, payload: json.monasteries})
       })
-  }
+  };
 }
 
 // 'ROOT_URL/monasteries/:id'
 export function fetchMonastery({id}) {
-  const url = `${ROOT_URL}/monasteries/${id}`;
-  return {
-    type: FETCH_MONASTERY,
-  }
+  return (dispatch) => {
+    dispatch({ type: FETCH_MONASTERY });
+    fetch(`${ROOT_URL}/monasteries/${id}`)
+      .then(response => {
+        return response.json()
+      })
+      .then(json => {
+        dispatch({ type:RECEIVED_MONASTERY, payload: json.monastery })
+      })
+  };
 }
+
+// 
