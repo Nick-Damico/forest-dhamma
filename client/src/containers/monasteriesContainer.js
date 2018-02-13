3b10701b7eee551474dffb5d0d1f856f23846791
Nicholas D'Amico <nickalan82@icloud.com>
2018-02-13 00:57:50 -0500

    rename file

---
+++ client/src/containers/monasteriesContainer.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchMonasteries, fetchMonastery } from '../actions/index';
+import MonasteryItem from '../components/monastery-item';
+
+class MonasteryContainer extends Component {
+  componentDidMount = () => {
+    this.props.fetchMonasteries();
+  }
+
+  onHandleClick = (selectedMonastery) => {
+    this.props.fetchMonastery(selectedMonastery);
+  }
+  componentDidUpdate() {
+    console.log('render');
+  }
+
+  render() {
+    const { monasteries } = this.props;
+    let monasteryItems;
+    if (monasteries) {
+      monasteryItems = monasteries.map((monastery, index) => {
+        return (
+          <MonasteryItem
+            key={index}
+            monastery={monastery}
+            onMonasteryClick={selectedMonastery => this.onHandleClick(selectedMonastery)}
+          />
+        )
+      })
+    }
+    return(
+      <div>
+        MonasteryList Page
+        {monasteryItems}
+      </div>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    monasteries: state.monasteryState.monasteries
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchMonasteries, fetchMonastery }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MonasteryContainer);
