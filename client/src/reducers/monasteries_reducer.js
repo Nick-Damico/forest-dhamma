465773ee606e759703c275c2cdaa5b0447f39bc5
Nicholas D'Amico <nickalan82@icloud.com>
2018-02-12 22:56:36 -0500

    Import variables from actions for use in switch statment

--- client/src/reducers/monasteries_reducer.js
+++ client/src/reducers/monasteries_reducer.js
@@ -1,8 +1,10 @@
+import { RECEIVED_MONASTERIES, FETCH_MONASTERIES }  from '../actions/index';
+
 function MonasteriesReducer(state = {loading: false, monasteries: []}, action) {
   switch( action.type ) {
-    case 'FETCH_MONASTERIES':
+    case FETCH_MONASTERIES:
       return Object.assign({}, state, {loading: true});
-    case 'RECEIVED_MONASTERIES':
+    case RECEIVED_MONASTERIES:
       return {loading: false, monasteries: action.payload}
     default:
       return state;
