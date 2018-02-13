8da754d36fc8d7b9a1fc6562265b32554d9664f3
Nicholas D'Amico <nickalan82@icloud.com>
2018-02-13 01:00:31 -0500

    update file path from name change

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import Home from './components/home';
-import MonasteryList from './containers/monasteries-list';
+import MonasteryList from './containers/monasteriesContainer';
+import TeachersContainer from './containers/teachersContainer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <MonasteryList />
+        <TeachersContainer />
       </div>
     );
   }
