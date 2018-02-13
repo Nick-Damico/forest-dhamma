bd4f26c243b4f49a25a360027aae7fbb1aabf5cb
Nicholas D'Amico <nickalan82@icloud.com>
2018-02-12 22:44:31 -0500

    add has_many relationship :teachers to return teacher data with instance of a monastery

--- app/serializers/monastery_serializer.rb
+++ app/serializers/monastery_serializer.rb
@@ -1,4 +1,5 @@
 class MonasterySerializer < ActiveModel::Serializer
   attributes :id, :name, :address, :site_url, :img_url
   has_many :talks, serializer: MonasteryTalkSerializer
+  has_many :teachers
 end
