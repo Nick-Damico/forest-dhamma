7d42314997ad88018644466e5fde5315f882be28
Nicholas D'Amico <nickalan82@icloud.com>
2018-02-14 10:54:01 -0500

    render teacherHeader and list of associatied teachers

--- client/src/containers/teachersContainer.js
+++ client/src/containers/teachersContainer.js
@@ -2,20 +2,35 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchTeachers } from '../actions/teacherActions';
-import TeachersHeader from '../'
+import TeachersHeader from '../components/teachers/teachersHeader';
+import TeachersList from '../components/teachers/teachersList';
 
 
 class TeachersContainer extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   componentDidMount() {
     this.props.fetchTeachers(this.props.match)
   }
 
   render() {
-    return <h1>Teachers of monastery</h1>
+    let teacherList;
+    const { teachers } = this.props;
+    const monastery = teachers ? teachers[teachers.length - 1] : null;
+    if ( teachers[0] ) {
+      if ( teachers[0].length > 1) {
+      teacherList = <TeachersList teachers={teachers[0]} />
+    } else {
+      teacherList = <h3>Currently no dhamma talks available for {monastery}</h3>;
+      }
+    }
+    return (
+      <div>
+          { teachers.length === 0
+            ? <h2>Loading</h2>  :
+            <TeachersHeader monastery={monastery} />
+          }
+          { teacherList }
+      </div>
+    )
   }
 }
 
@@ -24,7 +39,7 @@ function mapDispatchToProps(dispatch) {
 }
 
 function mapStateToProps(state) {
-  return { selectedMonastery: state.monasteryState.selectedMonastery }
+  return { teachers: state.teachers }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(TeachersContainer);
