65fd0424427f49de22e5de5ad10db0934e0bade5
Nicholas D'Amico <nickalan82@icloud.com>
2018-02-15 12:29:31 -0500

    add logic for sort, finding talk with most favorites, FavoriteTalk component working

--- client/src/components/talks/favoriteTalk.js
+++ client/src/components/talks/favoriteTalk.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { styles } from '../teachers/teacherItem';
 
-const FavoriteTalk = () => {
-  return (
-    <h3>Favorite Talk</h3>
+const FavoriteTalk = ({ talk }) => {
+  const { title, favorites, teacher } = talk;
+  const { name, profile_img } = teacher[0];
+  const { imgStyle, teacherList, itemDetails } = styles;
+  return(
+    <div>
+        <h3>Favorite Talk</h3>
+        <div className="teacherList-item" style={ teacherList }>
+          <img style={ imgStyle } src={ profile_img } alt={`profile pic of ${ name }`} />
+          <div className="teacherList-item--details" style={ itemDetails }>
+            <h4>{ title }</h4>
+            <span>{ name } - { favorites }</span>
+          </div>
+        </div>
+    </div>
   )
 }
 
--- client/src/containers/teachersContainer.js
+++ client/src/containers/teachersContainer.js
@@ -14,7 +14,7 @@ class TeachersContainer extends Component {
   }
 
   render() {
-    let teacherList, favorite, recentTalk;
+    let teacherList, favoriteTalk, recentTalk;
     const { teachers } = this.props;
     // Create a selectedMonastery is app state data on selecting a monastery to remove
     // overly complicated code below.
@@ -29,7 +29,12 @@ class TeachersContainer extends Component {
         let bDate = new Date(b.created_at);
         return bDate - aDate;
       })[0];
+      // set teacher as property on talks Array
       recentTalk.teacher = teachers[0].filter(teacher => teacher.id === recentTalk.teacher_id);
+      // store most favorited talk
+      favoriteTalk = talks.sort((a,b) => b.favorites - a.favorites)[0];
+      // set teacher as property on talks Array
+      favoriteTalk.teacher = teachers[0].filter(teacher => teacher.id === favoriteTalk.teacher_id);
     } else {
       teacherList = <h3>Currently no dhamma talks available for {monastery}</h3>;
       }
@@ -37,12 +42,13 @@ class TeachersContainer extends Component {
     return (
       <div>
           { teachers.length === 0
-            ? <h2>Loading</h2>  :
+            ? <h2>Loading</h2>
+            :
             <div>
-            <TeachersHeader monastery={monastery} />
-            { teacherList }
-            <RecentTalk talk={recentTalk}/>
-            <FavoriteTalk />
+              <TeachersHeader monastery={monastery} />
+              { teacherList }
+              <RecentTalk talk={recentTalk}/>
+              <FavoriteTalk talk={favoriteTalk}/>
             </div>
           }
 
