e7134ab378b547e0092e771d5ea0a5df90b564a3
Nicholas D'Amico <nickalan82@icloud.com>
2018-02-15 13:31:10 -0500

    clean up code

--- client/src/actions/teacherActions.js
+++ client/src/actions/teacherActions.js
@@ -6,13 +6,11 @@ export const RECEIVED_TEACHERS = 'RECEIVED_TEACHERS';
 const ROOT_URL = process.env.REACT_APP_API_URL;
 
 // 'ROOT_URL/monasteries/:id'
-export function fetchTeachers({url}) {
+export function fetchTeachers({ url }) {
   return (dispatch) => {
     dispatch({ type: FETCH_TEACHERS });
     fetch(`${ROOT_URL}${url}`)
-      .then(response => {
-        return response.json()
-      })
+      .then(response => return response.json())
       .then(json => {
         dispatch({ type:RECEIVED_TEACHERS, payload: json.monastery })
       })
