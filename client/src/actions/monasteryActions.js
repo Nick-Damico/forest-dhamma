773a8c958444d5890003fe5982af2b2adbde3c65
Nicholas D'Amico <nickalan82@icloud.com>
2018-02-15 13:31:54 -0500

    clean up code

--- client/src/actions/monasteryActions.js
+++ client/src/actions/monasteryActions.js
@@ -1,7 +1,5 @@
 import fetch from 'isomorphic-fetch';
 require('es6-promise').polyfill();
-
-// const ROOT_URL = 'http://localhost:3001/api/v1';
 export const FETCH_MONASTERIES = 'FETCH_MONASTERIES';
 export const RECEIVED_MONASTERIES = 'RECEIVED_MONASTERIES';
 const ROOT_URL = process.env.REACT_APP_API_URL;
@@ -11,9 +9,7 @@ export function fetchMonasteries() {
   return (dispatch) => {
     dispatch({type: FETCH_MONASTERIES});
     fetch(`${ROOT_URL}/monasteries`)
-      .then(response => {
-        return response.json()
-      })
+      .then(response => response.json())
       .then(json => {
         dispatch({type: RECEIVED_MONASTERIES, payload: json.monasteries})
       })
