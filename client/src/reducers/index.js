442340896148326e8c3dbd74c54c3f1e0ad0bb14
Nicholas D'Amico <nickalan82@icloud.com>
2018-02-15 13:32:47 -0500

    combineReducers add TalksReducer and talks property to Application Store

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import MonasteriesReducer from './monasteriesReducer';
 import TeachersReducer from './teachersReducer';
+import TalksReducer from './TalksReducer';
 
 const RootReducer = combineReducers({
   monasteries: MonasteriesReducer,
   teachers: TeachersReducer,
+  talks: TalksReducer,
 })
 
 export default RootReducer;
