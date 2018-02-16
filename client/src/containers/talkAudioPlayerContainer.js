3f4bed4062bc1ffd6251bbf2ec4f88a8aac530b7
Nicholas D'Amico <nickalan82@icloud.com>
2018-02-15 21:54:02 -0500

    add conditional for rendering loading message if API async hasn't finished, import and rendering TalksHeader component, define default props for component

--- client/src/containers/talkAudioPlayerContainer.js
+++ client/src/containers/talkAudioPlayerContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchSelectedTeacher } from '../actions/selectedTeacherActions';
+import TalksHeader from '../components/talks/talksHeader';
 
 class TalkAudioPlayerContainer extends Component {
   componentDidMount = () => {
@@ -9,17 +10,25 @@ class TalkAudioPlayerContainer extends Component {
   }
 
   render() {
-    const { selectedTeacher } = this.props;
-    console.log(selectedTeacher);
+    const { loading, teacher } = this.props;
+
+    if ( loading ) {
+      return <h2>Loading...</h2>;
+    }
+    const { monastery, talks } = teacher;
+
     return (
-      <h1>Talks Audio Player</h1>
+      <TalksHeader monastery={ monastery } teacher={ teacher } />
     )
   }
 }
 
+TalkAudioPlayerContainer.defaultProps = { loading: true, teacher: '' };
+
 function mapStateToProps(state) {
   return {
-    selectedTeacher: state.selectedTeacher
+    teacher: state.selectedTeacher.teacher,
+    loading: state.selectedTeacher.loading
    }
 }
 
@@ -28,3 +37,17 @@ function mapDispatchToProps(dispatch) {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(TalkAudioPlayerContainer);
+
+
+// todo: selecting a Most Favorited talk or Most Recent Talk needs to some way pass this info to this
+// Container render.
+
+// Fix for render once redux state is returned
+// render() {
+//   // Don't forget to connect the component to the Redux store yada yada
+//   const { loading } = this.props;
+//   return (
+//     !loading &&
+//     <MyComponent />
+//   );
+// }
