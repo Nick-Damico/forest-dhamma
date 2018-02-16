af0b7ad6d8dfb6cef2863c69eba1e255d2dac82d
Nicholas D'Amico <nickalan82@icloud.com>
2018-02-16 06:06:30 -0500

    inital build of upload audio form

---
+++ client/src/containers/uploadTalkContainer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class UploadTalkContainer extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <p>
+            Upload and Share <br />
+            Your favorite talks
+          </p>
+        </header>
+      </div>
+    )
+  }
+}
+
+export default UploadTalkContainer;
