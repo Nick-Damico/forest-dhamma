1c2e4b35bdd9fdabe2ef5ddd4e68df83e27d7791
Nicholas D'Amico <nickalan82@icloud.com>
2018-02-16 06:01:37 -0500

    add item/card for displaying songs

---
+++ client/src/components/talks/playlistItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import moment from 'moment';
+import { styles } from '../teachers/teacherItem';
+
+const playlistItem = ({ teacher, talk }) => {
+  const { name, profile_img } = teacher;
+  const { title, favorites, created_at } = talk;
+  const { imgStyle, teacherList, itemDetails } = styles;
+  return (
+    <div className="teacherList-item" style={ teacherList }>
+      <img style={ imgStyle } src={ profile_img } alt={`profile pic of ${ name }`} />
+      <div className="teacherList-item--details" style={ itemDetails }>
+        <h4>{ title }</h4>
+        <span>{ moment(created_at).format("MMM Do YYYY") } { `${favorites} favorites` }</span>
+      </div>
+    </div>
+  )
+}
+
+export default playlistItem;
