3429d91c3a03251bd480f3ff63ed6285545caab9
Nicholas D'Amico <nickalan82@icloud.com>
2018-02-15 21:48:03 -0500

    import Link from 'react-router..', const id from teacher props, add dynamic Link to path

--- client/src/components/talks/recentTalk.js
+++ client/src/components/talks/recentTalk.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import moment from 'moment';
+import { Link } from 'react-router-dom';
 import { styles } from '../teachers/teacherItem';
 
 const RecentTalk = ({ talk }) => {
   const { title, created_at, teacher } = talk;
-  const { name, profile_img } = teacher[0];
+  const { name, profile_img, id } = teacher[0];
   const { imgStyle, teacherList, itemDetails } = styles;
   return(
     <div>
@@ -14,6 +15,7 @@ const RecentTalk = ({ talk }) => {
           <div className="teacherList-item--details" style={ itemDetails }>
             <h4>{ title }</h4>
             <span>{ name } - { moment(created_at).format("MMM Do YYYY") }</span>
+            <Link to={`/teachers/${id}`}>talks</Link>
           </div>
         </div>
     </div>
