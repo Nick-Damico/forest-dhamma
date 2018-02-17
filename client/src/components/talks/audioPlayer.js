b784188d1825d1b73dc6b03ec3c337478d93d315
Nicholas D'Amico <nickalan82@icloud.com>
2018-02-17 09:47:06 -0500

    audio player component working, add shouldComponentUpdate to keep audio player from updating during other compoent re-renders on page

--- client/src/components/talks/audioPlayer.js
+++ client/src/components/talks/audioPlayer.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PlayerHeader from './playerHeader';
 
-const AudioPlayer = ({ talk, teacher }) => {
-  const { title, file_url } = talk;
-  return (
-    <div>
-      <PlayerHeader title={ title } teacher={ teacher }  />
-      <audio controls>
-        <source src={ file_url } type="audio/mpeg" />
-        Your Browser does not support the audio element
-      </audio>
-    </div>
-  )
+class AudioPlayer extends Component {
+  componentDidUpdate() {
+    this.player.pause();
+    this.player.load();
+    this.player.play();
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.talk !== this.props.talk
+  }
+
+  render() {
+    const { talk, teacher } = this.props;
+    const { title, file_url } = talk;
+    return (
+      <div>
+        <PlayerHeader title={ title } teacher={ teacher }  />
+        <audio id="audioPlayer" controls ref="audio" ref={(audio) => { this.player = audio; }}>
+          <source src={ file_url } type="audio/mpeg" />
+          Your Browser does not support the audio element
+        </audio>
+      </div>
+    )
+  }
 }
 
 export default AudioPlayer;
