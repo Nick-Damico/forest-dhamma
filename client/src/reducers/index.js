bd00f0e05c98d28571f4c807451c3987c9b97218
Nicholas D'Amico <nickalan82@icloud.com>
2018-02-17 10:07:27 -0500

    add property selectedTalk to receive store of selectedTalkReducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import MonasteriesReducer from './monasteriesReducer';
 import TeachersReducer from './teachersReducer';
 import SelectedTeacherReducer from './selectedTeacherReducer';
+import SelectedTalkReducer  from './selectedTalkReducer';
 
 const RootReducer = combineReducers({
   monasteries: MonasteriesReducer,
   teachers: TeachersReducer,
   selectedTeacher: SelectedTeacherReducer,
+  selectedTalk: SelectedTalkReducer,
 })
 
 export default RootReducer;
