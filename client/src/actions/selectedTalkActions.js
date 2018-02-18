df2bf220129274a45132a47a611ca28d4d62e648
Nicholas D'Amico <nickalan82@icloud.com>
2018-02-18 07:04:37 -0500

    action creator added for 'PATCH' request updating talk with a new Tag instance

--- client/src/actions/selectedTalkActions.js
+++ client/src/actions/selectedTalkActions.js
@@ -1,5 +1,6 @@
 export const ADD_SELECTED_TALK = 'ADD_SELECTED_TALK';
 export const REMOVE_SELECTED_TALK = 'REMOVE_SELECTED_TALK';
+const ROOT_URL = process.env.REACT_APP_API_URL;
 
 // 'ROOT_URL/monasteries/:id'
 export function addSelectedTalk(talk) {
@@ -12,4 +13,17 @@ export function removeSelectedTalk() {
   return (dispatch) => {
     dispatch({ type: REMOVE_SELECTED_TALK });
   };
+}
+
+export function addTagToTalk(talk, tag) {
+  return (dispatch) => {
+    fetch(`${ ROOT_URL }/talks/${talk.id}?tags_attributes[0][name]=${tag}`, {
+      method: 'PATCH',
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      })
+    })
+    .then(res => res.json())
+      .then(json => dispatch({ type: ADD_SELECTED_TALK, payload: json.talk }))
+  }
 }
