c35a35f3a7d4cd6e4e1a817ebab331c066173066
Nicholas D'Amico <nickalan82@icloud.com>
2018-02-17 19:59:41 -0500

    add accepts_nested_attributes_for :tags with a custom nested_attributes writer method for stopping duplicates

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -1,5 +1,15 @@
+require 'pry'
 class Talk < ApplicationRecord
   belongs_to :teacher
   has_many :tag_talks
   has_many :tags, through: :tag_talks
+  accepts_nested_attributes_for :tags
+
+    def tags_attributes=(tags_attributes)
+      tags_attributes.values.each do |tag_attribute|
+        tag = Tag.find_or_create_by(tag_attribute)
+        pry
+        self.tags << tag
+      end
+    end
 end
