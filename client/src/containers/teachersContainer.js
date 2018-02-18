d1cb8774cbda74b10055a6a25f41b5c9cc80f7fc
Nicholas D'Amico <nickalan82@icloud.com>
2018-02-18 08:59:00 -0500

    refactor of application state, mapStateToProps has  properties 'monastery, loading, teachers',  remove unnecessary complexity from render() logic and  returns.

--- client/src/containers/teachersContainer.js
+++ client/src/containers/teachersContainer.js
@@ -20,54 +20,55 @@ class TeachersContainer extends Component {
 
   render() {
     let teacherList, favoriteTalk, recentTalk;
-    const { teachers } = this.props;
-    // Create a selectedMonastery is app state data on selecting a monastery to remove
-    // overly complicated code below.
-    const monastery = teachers ? teachers[teachers.length - 1] : null;
-    if ( teachers[0] ) {
-      if ( teachers[0].length >= 1) {
-      teacherList = <TeachersList onHandleClick={this.onHandleClick} teachers={teachers[0]} />
-      // store most recent talk ? remove this step by fetch request for talks from server?
-      let talks = [].concat(...teachers[0].map(teacher => teacher.talks));
-      recentTalk = talks.sort((a,b) => {
-        let aDate = new Date(a.created_at);
-        let bDate = new Date(b.created_at);
-        return bDate - aDate;
-      })[0];
-      // set teacher as property on talks Array
-      recentTalk.teacher = teachers[0].filter(teacher => teacher.id === recentTalk.teacher_id);
-      // store most favorited talk
-      favoriteTalk = talks.sort((a,b) => b.favorites - a.favorites)[0];
-      // set teacher as property on talks Array
-      favoriteTalk.teacher = teachers[0].filter(teacher => teacher.id === favoriteTalk.teacher_id);
-    } else {
-      teacherList = <h3>Currently no dhamma talks available for {monastery}</h3>;
-      }
+    const { loading } = this.props;
+    if ( loading ) {
+      return <h2>loading...</h2>
     }
+
+    const { teachers, monastery } = this.props;
+    if( teachers.length === 0 ) {
+      teacherList = <h3>Currently no dhamma talks available for {monastery}</h3>;
+    }
+
+    teacherList = <TeachersList onHandleClick={this.onHandleClick} teachers={teachers} />
+    let talks = [].concat(...teachers.map(teacher => teacher.talks));
+    // Set favorite talk from collection
+    recentTalk = talks.sort((a,b) => {
+      let aDate = new Date(a.created_at);
+      let bDate = new Date(b.created_at);
+      return bDate - aDate;
+    })[0];
+    // set teacher as property on talks Array
+    recentTalk.teacher = teachers.filter(teacher => teacher.id === recentTalk.teacher_id);
+    // store most favorited talk
+    favoriteTalk = talks.sort((a,b) => b.favorites - a.favorites)[0];
+    // set teacher as property on talks Array
+    favoriteTalk.teacher = teachers.filter(teacher => teacher.id === favoriteTalk.teacher_id);
+
     return (
       <div>
-          { teachers.length === 0
-            ? <h2>Loading</h2>
-            :
-            <div>
-              <TeachersHeader monastery={monastery} />
-              { teacherList }
-              <RecentTalk onHandleClick={this.onHandleClick} talk={recentTalk}/>
-              <FavoriteTalk onHandleClick={this.onHandleClick} talk={favoriteTalk}/>
-            </div>
-          }
-
+        <TeachersHeader monastery={monastery} />
+        { teacherList }
+        <RecentTalk onHandleClick={this.onHandleClick} talk={recentTalk}/>
+        <FavoriteTalk onHandleClick={this.onHandleClick} talk={favoriteTalk}/>
       </div>
     )
   }
 }
+
+TeachersContainer.defaultProps = { loading: true };
 
 function mapDispatchToProps(dispatch) {
   return bindActionCreators({ fetchTeachers, addSelectedTalk, removeSelectedTalk }, dispatch)
 }
 
 function mapStateToProps(state) {
-  return { teachers: state.teachers }
+  const { teachers, monastery, loading } = state.teachers;
+  return {
+    teachers: teachers,
+    loading: loading,
+    monastery: monastery
+   }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(TeachersContainer);
