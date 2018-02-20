0d7e0891c507b5ab570f128d4efed5a2c581f2af
Nicholas D'Amico <nickalan82@icloud.com>
2018-02-20 10:59:24 -0500

    footer element added

---
+++ client/src/components/talks/talksFooter.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TalksFooter = () => {
+  return (
+    <footer className="playlist-footer">
+      <small>&copy;2018 ForestDhamma.All Rights Reserved.</small>
+    </footer>
+  )
+}
+
+export default TalksFooter;
