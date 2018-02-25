308b43ad8d5bae98a2f1e057510b1c9577610d50
Nicholas D'Amico <nickalan82@icloud.com>
2018-02-25 10:38:57 -0500

    add .wrapper class for containing elements

--- client/src/components/static/home.js
+++ client/src/components/static/home.js
@@ -16,22 +16,26 @@ const Home = () => {
       </header>
       <main className="landing-page--main">
         <section className="about-section--main">
-          <h3>Share Talks</h3>
-          <p>
-            Share the talks you love from your favorite teachers.
-          </p>
-          <img className="monitor-img" src={about} alt="" />
+          <div className="wrapper">
+            <h3>Share Talks</h3>
+            <p>
+              Share the talks you love from your favorite teachers.
+            </p>
+            <img className="monitor-img" src={about} alt="" />
+          </div>
         </section>
         <section className="discovery-section--main">
-          <h3>Discover Talks</h3>
-          <div className="discovery-section--text">
-            <img src={HeartIcon} alt="" className="heart-icon" />
-            <p>
-              Discover new talks and favorite the ones you love.
-            </p>
-          </div>
-          <div className="monk-icon--container">
-            <img src={MonkIcon} className="monk-icon" alt="" />
+          <div className="wrapper">
+            <h3>Discover Talks</h3>
+            <div className="discovery-section--text">
+              <img src={HeartIcon} alt="" className="heart-icon" />
+              <p>
+                Discover new talks and favorite the ones you love.
+              </p>
+            </div>
+            <div className="monk-icon--container">
+              <img src={MonkIcon} className="monk-icon" alt="" />
+            </div>
           </div>
         </section>
       </main>
