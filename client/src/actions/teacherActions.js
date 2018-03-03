f946c4862117d048a01fe1eafdc0d6c982f63419
Nicholas D'Amico <nickalan82@icloud.com>
2018-03-03 12:49:10 -0500

    remove const RECEIVED_TEACHERS_FROM_MONASTERY

--- client/src/actions/teacherActions.js
+++ client/src/actions/teacherActions.js
@@ -3,7 +3,6 @@ import { polyfill } from 'es6-promise';
 polyfill();
 export const FETCH_TEACHERS = 'FETCH_TEACHERS';
 export const RECEIVED_TEACHERS = 'RECEIVED_TEACHERS';
-export const RECEIVED_TEACHERS_FROM_MONASTERY = 'RECEIVED_TEACHERS_FROM_MONASTERY';
 const ROOT_URL = process.env.REACT_APP_API_URL;
 
 export function fetchTeachers() {
