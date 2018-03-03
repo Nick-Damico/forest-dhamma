b97cd8bf8cee7a1f927ab2cba89ef98db6b9d56d
Nicholas D'Amico <nickalan82@icloud.com>
2018-03-03 12:52:39 -0500

    removed selectedTeacherReducer.js on refactor

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,13 +1,11 @@
 import { combineReducers } from 'redux';
 import MonasteriesReducer from './monasteriesReducer';
 import TeachersReducer from './teachersReducer';
-import SelectedTeacherReducer from './selectedTeacherReducer';
 import SelectedTalkReducer  from './selectedTalkReducer';
 
 const RootReducer = combineReducers({
   monasteries: MonasteriesReducer,
   teachers: TeachersReducer,
-  selectedTeacher: SelectedTeacherReducer,
   selectedTalk: SelectedTalkReducer,
 })
 
