e267d87e59bd513410f7f974576e43714b2bd9e1
Nicholas D'Amico <nickalan82@icloud.com>
2018-03-04 13:37:27 -0500

    validates uniqueness_of talk_id :scope to tag_id, this fixes the bug of a tag only being associated to one talk, where tag should be unique to talk but can be associated to many talks

--- app/models/tag_talk.rb
+++ app/models/tag_talk.rb
@@ -2,5 +2,5 @@ class TagTalk < ApplicationRecord
   belongs_to :talk
   belongs_to :tag
 
-  validates_uniqueness_of :tag_id, :scope => :talk_id
+  validates_uniqueness_of :talk_id, :scope => :tag_id
 end
