1c2b8628fd153019fa8069030fe6cd36d8b2a330
Nicholas D'Amico <nickalan82@icloud.com>
2018-03-13 18:03:52 -0400

    remove useless consturctor method from class, added <button> element to rendered component, onHandleClick method attached as a 'click' event on new <button> element, onHandleClick is responsible for add css class to element for animation effect and dispatching the 'updateMonastery' action creator, shouldComponentUpdate operator added to decided if update is necessary

--- client/src/components/monasteries/monasteryItem.js
+++ client/src/components/monasteries/monasteryItem.js
@@ -1,60 +1,47 @@
 import React, {Component} from 'react';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import { Link } from 'react-router-dom';
-const ROOT_URL = process.env.REACT_APP_API_URL;
+
+import { updateMonastery } from '../../actions/monasteryActions';
+
 class MonasteryItem extends Component {
 
-
-  state = {
-    counter: 0,
+  onHandleClick = (e,monastery) => {
+    const target = e.target;
+    const likes = monastery.likes + 1;
+    // add animation class
+    target.classList.add('animate-heart');
+    // dispatch action creator
+    this.props.updateMonastery(monastery, likes);
+    // remove animation class
+    setTimeout(() => {
+      target.classList.remove('animate-heart');
+    }, 1300)
   }
 
-  onHandleClick = () => {
-    this.setState({
-      counter: this.state.counter += 1,
-    })
-  }
-
-  callApi = () => {
-    //prettier
-
-    console.log('a')
-    fetch(`${ROOT_URL}/monasteriesdsaasfdsfds`)
-      .then(response => {
-        if (response.status !== 200) {
-          throw new Error('baddd errorrrr!!!')
-        }
-
-        console.log('b')
-        return response.json()
-      })
-      .then(json => console.log('c', json))
-      .catch(err => console.log('d', err))
-      console.log('e')
-
-
-      // a e b c + json
-
-      // a e d + err
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.props.monastery !== nextProps.monastery ? true : false
   }
 
   render() {
-    const { monastery, onMonasteryClick, onHandleClick, counter } = this.props;
-
-    const { name, talks } = monastery;
+    const { monastery } = this.props;
+    const { name, talks, likes, img_url, id } = monastery;
     return (
-      <div className="monastery-container--item" style={{background: `url(${monastery.img_url})`, backgroundSize: 'cover'}}>
+      <div className="monastery-container--item" style={{background: `url(${ img_url })`, backgroundSize: 'cover'}}>
         <div className="monastery-container--item-inner">
           <h3 className="monastery-contianer--item-title">{ name }</h3>
-          <p>{talks.length} Talks</p>
-          <Link to={`/monasteries/${monastery.id}`} className="monastery-item--arrow">&#9660;</Link>
-          <span>{this.state.counter}</span>
-          <button onClick={this.onHandleClick}>+</button>
-          <button onClick={this.callApi}>Call Api</button>
+          <p>{ talks.length } Talks | { likes } Likes <button onClick={((e) => this.onHandleClick(e,monastery))} className="monastery-item__like-btn"></button></p>
+          <Link to={`/monasteries/${id}`} className="monastery-item--arrow">&#9660;</Link>
         </div>
-
       </div>
     )
   }
 }
 
-export default MonasteryItem;
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ updateMonastery }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(MonasteryItem);
