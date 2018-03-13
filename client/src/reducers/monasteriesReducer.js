c4c26c612ac6764d3c3bb41f10b3509cff1f2710
Nicholas D'Amico <nickalan82@icloud.com>
2018-03-13 18:09:55 -0400

    case for action.type UPDATE_MONASTERY added to handle the dispatch from action creator updateMonasteryreturns new updated list of monasteries to the redux Application store

--- client/src/reducers/monasteriesReducer.js
+++ client/src/reducers/monasteriesReducer.js
@@ -1,6 +1,7 @@
 import {
   RECEIVED_MONASTERIES,
-  FETCH_MONASTERIES
+  FETCH_MONASTERIES,
+  UPDATE_MONASTERY
 }  from '../actions/monasteryActions';
 
 function MonasteriesReducer(state = {
@@ -19,6 +20,9 @@ action) {
         collection: action.payload,
         isLoading: false
       }
+    case UPDATE_MONASTERY:
+      const newCollection = state.collection.map(m => m.id === action.payload.id ? action.payload : m)
+      return { collection: newCollection, isLoading: false };
     default:
       return state;
   }
