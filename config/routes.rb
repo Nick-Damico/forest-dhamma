7ac5292de33506333082ef9d226bb105b99bc64d
Nick-Damico <nickalan82@icloud.com>
2018-04-18 11:33:43 -0400

    remove scope of /v1 from api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace :api do
-    namespace :v1 do
+  scope '/scope' do
       resources :monasteries, only: [:index, :show, :update]
       resources :teachers
       resources :talks
-    end
   end
 
   get '*path', to: "application#fallback_index_html", constraints: ->(request) do
-  !request.xhr? && request.format.html?
-end
+    !request.xhr? && request.format.html?
+  end
 
 end
