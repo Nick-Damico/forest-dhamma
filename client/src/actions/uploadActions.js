c895a2f843dc033fb73b01e69af04ed7aa827b19
Nick-Damico <nickalan82@icloud.com>
2018-04-18 10:44:29 -0400

    fix error where string wasn't properly closed

--- client/src/actions/uploadActions.js
+++ client/src/actions/uploadActions.js
@@ -3,7 +3,7 @@ const ROOT_URL = process.env.REACT_APP_API_URL;
 
 export function uploadTalk(data) {
   return (dispatch) => {
-    fetch('/talks`, {
+    fetch(`/talks`, {
       method: 'POST',
       body: JSON.stringify(data),
       headers: {
